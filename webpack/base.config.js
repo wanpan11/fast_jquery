334c247bce4f66024a243725ffaada1e06559238
xglory_Wanp_win <965033410@qq.con>
2020-10-19 17:57:11 +0800

    updata

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,13 +1,11 @@
 const path = require('path')  //path是node.js自带的路径工具
 const entry = require('./entry');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     context: path.resolve(process.cwd(), "src/app"),
     entry: entry,
     watch: true,
-    plugins: [
-        new ExtractTextPlugin("style.css"),
-    ],
     output: {
         path: path.resolve(process.cwd(), "dist"),  //path.resolve(process.cwd()，) 指当前node启动目录
         filename: "[name].js"     //输出文件name
@@ -41,9 +39,28 @@ module.exports = {
                 })
             },
             {
-                test: /\.(.png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
-                loader: "file-loader?name=[name]_[sha512:hash:base64:7].[ext]"
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: "file-loader",
+                options: {
+                    name: 'assets/[name]_[sha512:hash:base64:7].[ext]'
+                }
             }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin("css/[name].css"),
+        new HtmlWebpackPlugin({
+            title: '首页',
+            template: path.resolve(
+                process.cwd(),
+                'src/base/webpack.template.html'),
+            filename: 'index.html',
+            chunks: ['index']  //sale.html引入的css和js文件资源
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(process.cwd(), "index.html"),
+        compress: true,
+        port: 9000
+    }
 }
